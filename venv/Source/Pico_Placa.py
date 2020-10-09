27c111786695300f742d361abce046a635022698
byrpatrick <byrontbc@hotmail.com>
2020-10-08 22:24:11 -0500

    First function based on last digit, day and a given time.

---
+++ venv/Source/Pico_Placa.py
@@ -0,0 +1,21 @@
+from datetime import time
+
+rulesPicoPlaca = {"Monday":[1,2], "Tuesday":[3,4], "Wednesday":[5,6], "Thursday":[7,8], "Friday":[9,0]}
+
+def isntPicoHour(timeGiven):
+    return (timeGiven >= time(7, 0) and timeGiven <= time(9, 30) or
+            timeGiven >= time(16, 0) and timeGiven <= time(19, 30))
+
+def canBeOnRoad(lastDigit, day, timeGiven):
+    #It's weekend anyone can be on road, enjoy your journey!!
+    if day in ["Saturday","Sunday"]:
+        return "The car can be on road."
+    #When the time is right, you can drive!!
+    #If isn't a pico hour you can drive.
+    if isntPicoHour(timeGiven):
+        return "The car can be on road."
+    #Isn't weekend and is a pico hour. Can I drive?
+    for k, v in rulesPicoPlaca.items():
+        if (day == k) and (lastDigit in v):
+            return "The car can't be on road."
+    return "The car can be on road."
